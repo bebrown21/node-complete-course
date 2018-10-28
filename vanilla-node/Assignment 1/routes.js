de8d126b58e6a253d9a1783001360582f5c8b832
Brandon Brown <Brandon.Brown@metova.com>
2018-10-27 19:08:50 -0500

    initialize node app

---
+++ vanilla-node/Assignment 1/routes.js
@@ -0,0 +1,34 @@
+const requestHandler = (req, res) => {
+    const url = req.url;
+    const method = req.method;
+
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<body><h1>Hello!</h1> <form action="/create-user" method="POST"><input type="text" name="username"/> <button type="submit">Submit</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    if(url === '/create-user' && method === 'POST') {
+        const body = [];
+        console.log(req);
+        req.on('data', chunk => {
+            body.push(chunk);
+        })
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            console.log(message);
+            res.statusCode = 302;
+            res.setHeader('Location', '/');
+            return res.end();
+        })
+    }
+    if (url === '/users') {
+        res.write('<html>');
+        res.write('<body><ul><li>User 1</li><li>User 2</li></ul></body>');
+        res.write('</html>');
+        return res.end();
+    }
+}
+
+module.exports = requestHandler;
