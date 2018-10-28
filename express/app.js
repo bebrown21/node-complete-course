2ae83b9c50200ffd0c10e64894ece92b0eee67aa
Brandon Brown <Brandon.Brown@metova.com>
2018-10-28 13:02:41 -0500

    refactor views using ejs instead of pug or handlebars

--- express/app.js
+++ express/app.js
@@ -2,20 +2,11 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHandlebars = require('express-handlebars');
 
 const app = express();
 
 /* Defines our templating engine and views dir */
-app.engine(
-  'hbs',
-  expressHandlebars({
-    layoutsDir: 'views/layouts/',
-    defaultLayout: 'main-layout',
-    extname: 'hbs'
-  })
-);
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
@@ -27,7 +18,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes.routes);
 app.use(shopRoutes);
 app.use((req, res, next) => {
-  res.status(404).render('page-not-found', { pageTitle: 'Page Not Found' });
+  res.status(404).render('page-not-found', { pageTitle: 'Page Not Found', path: '' });
 });
   
 app.listen(3000);
