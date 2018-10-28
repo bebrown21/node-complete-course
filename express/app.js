4881feb086fc834f612533cdb8a257fd93838feb
Brandon Brown <Brandon.Brown@metova.com>
2018-10-28 11:48:45 -0500

    add pug templating files and configuration

--- express/app.js
+++ express/app.js
@@ -5,16 +5,20 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+/* Defines our templating engine and views dir */
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminRoutes.routes);
 app.use(shopRoutes);
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', 'page-not-found.html'));
+  res.status(404).render('page-not-found', { pageTitle: 'Page Not Found' });
 });
-
+  
 app.listen(3000);
--- express/routes/shop.js
+++ express/routes/shop.js
@@ -2,11 +2,13 @@ const path = require('path');
 const express = require('express');
 
 const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  const products = adminData.products;
+  res.render('shop', { prods: products, pageTitle: 'Shop', path: '/' }); //we defined our templating engine and views dir in app.js
 });
 
 module.exports = router;
