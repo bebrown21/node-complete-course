ad9f615373bc03304fa3665680fce63c2be4a9c4
Brandon Brown <Brandon.Brown@metova.com>
2018-11-09 20:23:23 -0600

    refactor using mongoose

--- mongoDB/app.js
+++ mongoDB/app.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
 const User = require('./models/user');
 
 const app = express();
@@ -19,9 +19,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById('5be3718b45d7214491da3af8')
+  User.findById('5be62c683530464238750b71')
     .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
+      req.user = user;
       next();
     })
     .catch(err => console.log(err));
@@ -32,6 +32,23 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(3000);
-});
+mongoose.connect('mongodb+srv://Brandon:123@cluster0-wdjil.mongodb.net/node-complete-course?retryWrites=true', { useNewUrlParser: true })
+  .then(result => {
+    console.log('Connected to Database!!!')
+
+    User.findOne()
+      .then(user => {
+        if (!user) {
+          const user = new User({
+            name: 'Bob',
+            email: 'test@email.com',
+            cart: {
+              items: []
+            }
+          });
+          user.save();
+        }
+      })
+    app.listen(3000);
+  }) 
+  .catch(err => console.log(err));
