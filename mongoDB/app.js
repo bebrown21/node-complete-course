358cf91ef82a32032dbc1e95a1304f347afeb42e
Brandon Brown <Brandon.Brown@metova.com>
2018-11-14 16:12:30 -0600

    add csrf protection

--- mongoDB/app.js
+++ mongoDB/app.js
@@ -5,6 +5,7 @@ const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session); //pass the above session
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const csrf = require('csurf');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -16,6 +17,7 @@ const store = new MongoDBStore({
   uri: MONGODB_URI,
   collection: 'sessions'
 });
+const csrfProtection = csrf();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -32,6 +34,7 @@ app.use(session({
   saveUninitialized: false, //similar to above. Will not save if nothing has changed
   store: store
 })); //there are additional settings you can add for Cookies as well
+app.use(csrfProtection);
 
 app.use((req, res, next) => {
   if (!req.session.user) {
@@ -43,6 +46,12 @@ app.use((req, res, next) => {
       next();
     })
     .catch(err => console.log(err));
+})
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
 })
 
 app.use('/admin', adminRoutes);
