d08f12b40a487ab58deb4f66611d611e4b476064
Brandon Brown <Brandon.Brown@metova.com>
2018-11-27 20:22:59 -0600

    implement socket.io for posts

--- node-rest-api/controllers/feed.js
+++ node-rest-api/controllers/feed.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const { validationResult } = require('express-validator/check');
+const io = require('../socket');
 const Post = require('../models/post');
 const User = require('../models/user');
 
@@ -14,6 +15,8 @@ exports.getPosts = (req, res, next) => {
     .then(count => {
       totalItems = count;
       return Post.find()
+        .populate('creator')
+        .sort({ createdAt: -1 })
         .skip((currentPage - 1) * perPage)
         .limit(perPage);
     })
@@ -62,6 +65,16 @@ exports.createPost = (req, res, next) => {
       return user.save();
     })
     .then(result => {
+      io.getIO().emit('posts', { 
+        action: 'create', 
+        post: { 
+          ...post._doc, 
+          creator: { 
+            _id: req.userId, 
+            name: creator.name 
+          }
+        } 
+      });
       res.status(201).json({
         message: 'Post created successfully',
         post: post,
@@ -119,13 +132,14 @@ exports.updatePost = (req, res, next) => {
     throw error;
   }
   Post.findById(postId)
+    .populate('creator')
     .then(post => {
       if (!post) {
         const error = new Error('Could not find Post!')
         error.statusCode = 404;
         throw error;
       }
-      if (post.creator.toString() !== req.userId) {
+      if (post.creator._id.toString() !== req.userId) {
         const error = new Error('Not Authorized!');
         error.statusCode = 403;
         throw error;
@@ -139,6 +153,7 @@ exports.updatePost = (req, res, next) => {
       return post.save();
     })
     .then(result => {
+      io.getIO().emit('posts', { action: 'update', post: result } )
       res.status(200).json({
         message: 'Post updated!',
         post: result
@@ -160,8 +175,9 @@ exports.deletePost = (req, res, next) => {
         const error = new Error('Could not find Post!')
         error.statusCode = 404;
         throw error;
-      }
-      if (post.creator.toString() !== req.userId) {
+      } 
+      console.log(post.creator, req)
+      if (post.creator._id.toString() !== req.userId) {
         const error = new Error('Not Authorized!');
         error.statusCode = 403;
         throw error;
@@ -177,6 +193,10 @@ exports.deletePost = (req, res, next) => {
       return user.save();
     })
     .then(result => {
+      io.getIO().emit('posts', {
+        action: 'delete',
+        post: postId
+      })
       res.status(200).json({
         message: 'Deleted Post!'
       })
